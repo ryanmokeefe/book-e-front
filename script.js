643dfecc481f88ccd1a1bc41bcefb5c76bd132d6
superbuggy <jpqreichard@gmail.com>
2017-12-21 09:17:59 -0500

    add exercise set up

--- script.js
+++ script.js
@@ -6,9 +6,26 @@ $('form.post').submit(handlePost)
 $('form.update').submit(handleUpdate)
 $(document).ready(loadData)
 
+function handlePost (event) {
+  event.preventDefault()
+  const bookmarkTitle = $('.post input[name="title"]').val()
+  const bookmarkUrl = $('.post input[name="url"]').val()
+  // Use $.ajax to make a POST request here
+}
+
+function handleUpdate (event) {
+  // Use $.ajax to make a PUT request here
+  // itemUnderEdit will contain the original values of the item being edited
+}
+
+function handleDelete (event) {
+  // Use $.ajax to make a DELETE request here
+}
+
 function loadData () {
   $('ul').html('')
   let data = $.getJSON(serverUrl).then(response => {
+    // Take data in db and create a tags and 2 buttons for deletion and showing the edit/update form
     response.forEach(bookmark => {
       const listItemFeatLink = $(`<li><a href="${bookmark.url}">${bookmark.title}</a></li>`)
       const updateLink = $(`<button class="update" data-title="${bookmark.title}"> Update </button>`)
@@ -19,79 +36,22 @@ function loadData () {
     })
 
     $('button.update').click(event => {
-      isUserEditing = !isUserEditing
+      isUserEditing = !isUserEditing // toggles the boolean value of isUserEditing
       const buttonText = isUserEditing ? 'Cancel' : 'Update'
       $(event.target).html(buttonText)
-      $('form').toggleClass('hidden')
+
+      $('form').toggleClass('hidden') // hides post form and shows the editing form
       let currentTitle = $(event.target).siblings('a').html()
       let currentUrl = $(event.target).siblings('a').attr('href')
       itemUnderEdit = {
         title: currentTitle,
         url: currentUrl
       }
+      // Populates the form with the values of the Bookmark being edited
       $('.update input[name="title"]').val(currentTitle)
       $('.update input[name="url"]').val(currentUrl)
     })
 
-    $('button.delete').click(event => {
-      let title = $(event.target).attr('data-title')
-      const deleteRequestContent = {
-        url: serverUrl + title,
-        type: 'DELETE',
-        dataType: 'json'
-      }
-      $.ajax(deleteRequestContent)
-        .then(loadData)
-        .fail(error => console.error(error))
-    })
+    $('button.delete').click(handleDelete)
   })
-}
-
-function handlePost (event) {
-  event.preventDefault()
-  const bookmarkTitle = $('.post input[name="title"]').val()
-  const bookmarkUrl = $('.post input[name="url"]').val()
-
-  const newData = {
-    title: bookmarkTitle,
-    url: bookmarkUrl
-  }
-
-  const requestContent = {
-    url: serverUrl,
-    type: 'POST',
-    dataType: 'json',
-    data: newData
-  }
-
-  $.ajax(requestContent)
-    .then(loadData)
-    .fail(error => console.error(error))
-}
-
-function handleUpdate (event) {
-  event.preventDefault()
-  const bookmarkTitle = $('.update input[name="title"]').val()
-  const bookmarkUrl = $('.update input[name="url"]').val()
-
-  const newData = {
-    title: bookmarkTitle,
-    url: bookmarkUrl
-  }
-
-  const requestContent = {
-    url: serverUrl + itemUnderEdit.title,
-    type: 'PUT',
-    dataType: 'json',
-    data: newData
-  }
-
-  $.ajax(requestContent)
-    .then(() => {
-      isUserEditing = !isUserEditing
-      $('button.update').html('Update')
-      $('form').toggleClass('hidden')
-      loadData()
-    })
-    .fail(error => console.error(error))
 }
